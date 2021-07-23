e7e1127d853249cc1fe9b8d83c5d4aec770b3b19
saahil0826 <snawalkha0826@gmail.om>
2021-07-24 01:19:38 +0530

    Added color slider and styled

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Palette {...seedColors[2]} />
+      <Palette palette={generatePalette(seedColors[4])} />
       </div>
     );
   }
--- src/Palette.js
+++ src/Palette.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorBox from "./ColorBox.js";
+import "rc-slider/assets/index.css";
 import "./Palette.css";
+import Slider from "rc-slider";
 
-function Palette({ colors }) {
+function Palette({ palette }) {
+  const [level, setLevel] = useState(500);
+
+  const changeLevel = level => {
+    setLevel(level);
+  };
   return (
     <div className="Palette">
-      {/* Navbar goes here */}
+      <div className="slider">
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          step={100}
+          onAfterChange={changeLevel}
+        />
+      </div>
       <div className="Palette-colors">
-        {colors.map(color => (
-          <ColorBox background={color.color} name={color.name} />
+        {palette.colors[level].map(color => (
+          <ColorBox background={color.hex} name={color.name} />
         ))}
       </div>
       {/* footer eventually */}

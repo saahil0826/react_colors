494f85ef95e6151dedd4dbe6bff6a58a7298aabf
saahil082 <snawalkha0826@gmail.com>
2021-09-09 14:33:24 +0530

    Make PaletteList responsive

--- src/styles/PaletteList.js
+++ src/styles/PaletteList.js
@@ -1,3 +1,5 @@
+import sizes from "./sizes";
+
 const styles = {
   root: {
     backgroundColor: "blue",
@@ -11,7 +13,13 @@ const styles = {
     display: "flex",
     alignItems: "flex-start",
     flexDirection: "column",
-    flexWrap: "wrap"
+    flexWrap: "wrap",
+    [sizes.down("xl")]: {
+      width: "80%"
+    },
+    [sizes.down("xs")]: {
+      width: "75%"
+    }
   },
   nav: {
     display: "flex",
@@ -28,7 +36,14 @@ const styles = {
     width: "100%",
     display: "grid",
     gridTemplateColumns: "repeat(3, 30%)",
-    gridGap: "5%"
+    gridGap: "2.5rem",
+   [sizes.down("md")]: {
+     gridTemplateColumns: "repeat(2, 50%)"
+   },
+   [sizes.down("xs")]: {
+     gridTemplateColumns: "repeat(1, 100%)",
+     gridGap: "1rem"
+   }
   }
 };
 

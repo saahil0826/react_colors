33ec593625add2ad19288d9d9ee2d6f3c2f78893
saahil082 <snawalkha0826@gmail.com>
2021-09-09 14:53:48 +0530

    Animate palette deletion

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import MiniPalette from "./MiniPalette.js";
 import { withStyles } from "@material-ui/styles";
 import styles from "./styles/PaletteList.js";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 function PaletteList(props) {
   const goToPalette = (id) => {
@@ -13,20 +14,22 @@ function PaletteList(props) {
     <div className={classes.root}>
       <div className={classes.container}>
         <nav className={classes.nav}>
-        <h1 className={classes.heading}>React Colors</h1>
+          <h1 className={classes.heading}>React Colors</h1>
           <Link to="/palette/new">Create Palette</Link>
         </nav>
-        <div className={classes.palettes}>
+        <TransitionGroup className={classes.palettes}>
           {palettes.map((palette) => (
-            <MiniPalette
-              {...palette}
-              handleClick={() => goToPalette(palette.id)}
-              handleDelete={deletePalette}
-              key={palette.id}
-              id={palette.id}
-            />
+            <CSSTransition key={palette.id} classNames="fade" timeout={500}>
+              <MiniPalette
+                {...palette}
+                handleClick={() => goToPalette(palette.id)}
+                handleDelete={deletePalette}
+                key={palette.id}
+                id={palette.id}
+              />
+            </CSSTransition>
           ))}
-        </div>
+        </TransitionGroup>
       </div>
     </div>
   );
--- src/styles/PaletteList.js
+++ src/styles/PaletteList.js
@@ -2,6 +2,15 @@ import sizes from "./sizes";
 import bg from "./bg.svg";
 
 const styles = {
+  "@global": {
+    ".fade-exit": {
+      opacity: 1,
+    },
+    ".fade-exit-active": {
+      opacity: 0,
+      transition: "opacity 500ms ease-out",
+    },
+  },
   root: {
     height: "100vh",
     display: "flex",
@@ -10,10 +19,10 @@ const styles = {
     /* background by SVGBackgrounds.com */
     backgroundColor: "#0B01FF",
     backgroundImage: `url(${bg})`,
-    overflow: "scroll"
+    overflow: "scroll",
   },
   heading: {
-    fontSize: "2rem"
+    fontSize: "2rem",
   },
   container: {
     width: "50%",
@@ -22,11 +31,11 @@ const styles = {
     flexDirection: "column",
     flexWrap: "wrap",
     [sizes.down("xl")]: {
-      width: "80%"
+      width: "80%",
     },
     [sizes.down("xs")]: {
-      width: "75%"
-    }
+      width: "75%",
+    },
   },
   nav: {
     display: "flex",
@@ -35,8 +44,8 @@ const styles = {
     alignItems: "center",
     color: "white",
     "& a": {
-      color: "white"
-    }
+      color: "white",
+    },
   },
   palettes: {
     boxSizing: "border-box",
@@ -44,14 +53,14 @@ const styles = {
     display: "grid",
     gridTemplateColumns: "repeat(3, 30%)",
     gridGap: "2.5rem",
-   [sizes.down("md")]: {
-     gridTemplateColumns: "repeat(2, 50%)"
-   },
-   [sizes.down("xs")]: {
-     gridTemplateColumns: "repeat(1, 100%)",
-     gridGap: "1.4rem"
-   }
-  }
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2, 50%)",
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "repeat(1, 100%)",
+      gridGap: "1.4rem",
+    },
+  },
 };
 
 export default styles;

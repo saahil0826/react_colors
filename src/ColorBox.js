a5f98bdea4c74ab9340a2330214793e37c84f8d6
saahil082 <snawalkha0826@gmail.com>
2021-09-09 13:44:13 +0530

    Add responsive helper and style ColorBox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
-import "./ColorBox.css";
-import chroma from "chroma-js";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
+import styles from "./styles/ColorBox.js";
+import { withStyles } from "@material-ui/styles";
 
-function ColorBox({ background, name, moreUrl, showingFullPalette }) {
+function ColorBox(props) {
   const [copied, setCopied] = useState(false);
-  const isDarkColor = chroma(background).luminance() <= 0.08;
-  const isLightColor = chroma(background).luminance() >= 0.7;
+  // const isDarkColor = chroma(background).luminance() <= 0.08;
+  // const isLightColor = chroma(background).luminance() >= 0.7;
 
   const changeCopyState = () => {
     setCopied(true);
@@ -19,35 +19,37 @@ function ColorBox({ background, name, moreUrl, showingFullPalette }) {
     }, 1500);
   }, [copied]);
 
+  const { background, name, moreUrl, showingFullPalette, classes } = props;
+
   return (
     <CopyToClipboard text={background} onCopy={changeCopyState}>
-      <div style={{ background }} className="ColorBox">
-        <div
-          style={{ background }}
-          className={`copy-overlay ${copied && "show"}`}
-        />
-        <div className={`copy-msg ${copied && "show"}`}>
-          <h1>copied!</h1>
-          <p className={isLightColor && "dark-text"}>{background}</p>
-        </div>
-        <div className="copy-container">
-          <div className="box-content">
-            <span className={isDarkColor && "light-text"}>{name}</span>
-          </div>
-          <button className={`copy-button ${isLightColor && "dark-text"}`}>
-            Copy
-          </button>{" "}
-        </div>
-        {showingFullPalette && (
-          <Link to={moreUrl} onClick={(e) => e.stopPropagation()}>
-            <span className={`see-more ${isLightColor && "dark-text"}`}>
-              MORE
-            </span>{" "}
-          </Link>
-        )}
-      </div>
-    </CopyToClipboard>
+         <div style={{ background }} className={classes.ColorBox}>
+           <div
+             style={{ background }}
+             className={`${classes.copyOverlay} ${copied &&
+               classes.showOverlay}`}
+           />
+           <div
+             className={`${classes.copyMessage} ${copied &&
+               classes.showMessage}`}
+           >
+             <h1>copied!</h1>
+             <p className={classes.copyText}>{background}</p>
+           </div>
+           <div>
+             <div className={classes.boxContent}>
+               <span className={classes.colorName}>{name}</span>
+             </div>
+             <button className={classes.copyButton}>Copy</button>
+           </div>
+           {showingFullPalette && (
+             <Link to={moreUrl} onClick={e => e.stopPropagation()}>
+               <span className={classes.seeMore}>MORE</span>
+             </Link>
+           )}
+         </div>
+       </CopyToClipboard>
   );
 }
 
-export default ColorBox;
+export default withStyles(styles)(ColorBox);

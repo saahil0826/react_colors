bd0399b8d261d61a406be4c48b6214c2c8c1adb1
saahil082 <snawalkha0826@gmail.com>
2021-09-09 14:49:26 +0530

    Add bg svg to PaletteList

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -13,7 +13,7 @@ function PaletteList(props) {
     <div className={classes.root}>
       <div className={classes.container}>
         <nav className={classes.nav}>
-          <h1>React Colors</h1>
+        <h1 className={classes.heading}>React Colors</h1>
           <Link to="/palette/new">Create Palette</Link>
         </nav>
         <div className={classes.palettes}>
--- src/styles/PaletteList.js
+++ src/styles/PaletteList.js
@@ -1,12 +1,19 @@
 import sizes from "./sizes";
+import bg from "./bg.svg";
 
 const styles = {
   root: {
-    backgroundColor: "blue",
     height: "100vh",
     display: "flex",
     alignItems: "flex-start",
-    justifyContent: "center"
+    justifyContent: "center",
+    /* background by SVGBackgrounds.com */
+    backgroundColor: "#0B01FF",
+    backgroundImage: `url(${bg})`,
+    overflow: "scroll"
+  },
+  heading: {
+    fontSize: "2rem"
   },
   container: {
     width: "50%",
@@ -42,7 +49,7 @@ const styles = {
    },
    [sizes.down("xs")]: {
      gridTemplateColumns: "repeat(1, 100%)",
-     gridGap: "1rem"
+     gridGap: "1.4rem"
    }
   }
 };

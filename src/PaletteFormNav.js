919646aa9d1e7cf84c11f9b8351f67e89df594fb
saahil082 <snawalkha0826@gmail.com>
2021-09-09 11:58:06 +0530

    Tweak styles of form

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -7,6 +7,8 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import Button from "@material-ui/core/Button";
 import PaletteMetaForm from "./PaletteMetaForm";
 import { withStyles } from "@material-ui/core/styles";
@@ -39,9 +41,10 @@ function PaletteFormNav(props) {
             color="inherit"
             aria-label="Open drawer"
             onClick={props.handleDrawerOpen}
-            className={classNames(classes.menuButton, open && classes.hide)}
-          >
-            <MenuIcon />
+            className={classNames(classes.menuButton, {
+                           [classes.hide]: open
+                         })}          >
+                         <AddToPhotosIcon />
           </IconButton>
           <Typography variant="h6" color="inherit" noWrap>
             Create A Palette

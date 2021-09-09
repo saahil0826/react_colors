a33f2f9258d34ec1ac0ff3a0d54e07abc5169ba4
saahil082 <snawalkha0826@gmail.com>
2021-09-09 11:52:55 +0530

    Move styles out and add constants file

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -2,24 +2,9 @@ import React, { useState, useEffect } from "react";
 import Button from "@material-ui/core/Button";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 import { ChromePicker } from "react-color";
+import styles from "./styles/ColorPickerForm.js";
 import { withStyles } from "@material-ui/core/styles";
 
-const styles = {
-  picker: {
-    width: "100% !important",
-    marginTop: "2rem",
-  },
-  addColor: {
-    width: "100%",
-    padding: "1rem",
-    marginTop: "1rem",
-    fontSize: "2rem",
-  },
-  colorNameInput: {
-    width: "100%",
-    height: "70px",
-  },
-};
 
 function ColorPickerForm(props) {
   const [currentColor, setCurrentColor] = useState("teal");
--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -11,48 +11,9 @@ import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import Button from "@material-ui/core/Button";
 import DraggableColorList from "./DraggableColorList.js";
 import { arrayMoveImmutable } from "array-move";
+import styles from "./styles/NewPaletteForm.js";
 
-const drawerWidth = 400;
 
-const styles = (theme) => ({
-  root: {
-    display: "flex",
-  },
-  hide: {
-    display: "none",
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: "0 8px",
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end",
-  },
-  content: {
-    flexGrow: 1,
-    height: "calc(100vh - 64px)",
-    padding: theme.spacing(3),
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: -drawerWidth,
-  },
-  contentShift: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  },
-});
 
 function NewPaletteForm(props) {
   const [open, setOpen] = useState(true);
